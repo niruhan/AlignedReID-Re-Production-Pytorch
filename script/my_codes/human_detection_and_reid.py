65f87c86d297083db48c255c3af94d390f5c7ff5
niruhan <niruhanv@gmail.com>
2018-09-12 22:44:59 +0530

    combining human detection and global feature extraction

--- script/my_codes/human_detection_and_reid.py
+++ script/my_codes/human_detection_and_reid.py
@@ -1,12 +1,17 @@
-# Code adapted from Tensorflow Object Detection Framework
-# https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
-# Tensorflow Object Detection Detector
-
 import numpy as np
 import tensorflow as tf
 import cv2
 import time
 
+from aligned_reid.model.Model import Model
+from torch.nn.parallel import DataParallel
+import torch.optim as optim
+import torch
+from aligned_reid.utils.utils import load_state_dict
+from aligned_reid.utils.utils import set_devices
+from torch.autograd import Variable
+
+# human detection part
 
 class DetectorAPI:
     def __init__(self, path_to_ckpt):
@@ -65,7 +70,7 @@ if __name__ == "__main__":
     threshold = 0.7
     # cap = cv2.VideoCapture('/path/to/input/video')
 
-    img = cv2.imread('my_images/pedestrians.jpg')
+    img = cv2.imread('my_images/sutha.jpg')
 
     # img = cv2.resize(cap, (1280, 720))
 
@@ -78,26 +83,72 @@ if __name__ == "__main__":
         if classes[i] == 1 and scores[i] > threshold:
             box = boxes[i]
             cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+            print box[1]
+            person_image = img[box[0]:box[2], box[1]:box[3]]
 
-    cv2.imshow("preview", img)
-    key = cv2.waitKey(0)
+    # cv2.imshow("preview", img)
+    # key = cv2.waitKey(0)
+    # cv2.imshow("preview", person_image)
+    # key = cv2.waitKey(0)
 
-    # while True:
-    #     r, img = cap.read()
-    #     img = cv2.resize(img, (1280, 720))
-    #
-    #     boxes, scores, classes, num = odapi.processFrame(img)
-    #
-    #     # Visualization of the results of a detection.
-    #
-    #     for i in range(len(boxes)):
-    #         # Class 1 represents human
-    #         if classes[i] == 1 and scores[i] > threshold:
-    #             box = boxes[i]
-    #             cv2.rectangle(img,(box[1],box[0]),(box[3],box[2]),(255,0,0),2)
-    #
-    #     cv2.imshow("preview", img)
-    #     key = cv2.waitKey(1)
-    #     if key & 0xFF == ord('q'):
-    #         break
+
+
+# extracting global features for person reid
+
+input_image = person_image
+resized_image = cv2.resize(input_image, (128, 256))
+
+
+# input_image = np.asarray(Image.open('gow_query.jpg'))
+transposed = resized_image.transpose(2,0,1)
+test_img = transposed[np.newaxis]
+
+# cv2.imshow("preview", input_image)
+# key = cv2.waitKey(0)
+
+###########
+# Models  #
+###########
+local_conv_out_channels = 128
+num_classes = 3
+
+model = Model(local_conv_out_channels=local_conv_out_channels, num_classes=num_classes)
+# Model wrapper
+model_w = DataParallel(model)
+
+base_lr = 2e-4
+weight_decay = 0.0005
+optimizer = optim.Adam(model.parameters(), lr = base_lr, weight_decay = weight_decay)
+
+# Bind them together just to save some codes in the following usage.
+modules_optims = [model, optimizer]
+
+model_weight_file = '../../model_weight.pth'
+
+map_location = (lambda storage, loc: storage)
+sd = torch.load(model_weight_file, map_location=map_location)
+load_state_dict(model, sd)
+print('Loaded model weights from {}'.format(model_weight_file))
+
+sys_device_ids = (0,)
+
+TVT, TMO = set_devices(sys_device_ids)
+
+old_train_eval_model = model.training
+
+# Set eval mode.
+# Force all BN layers to use global mean and variance, also disable dropout.
+model.eval()
+
+ims = np.stack(input_image, axis=0)
+
+ims = Variable(TVT(torch.from_numpy(test_img).float()))
+
+global_feat, local_feat = model(ims)[:2]
+global_feat = global_feat.data.cpu().numpy()
+local_feat = local_feat.data.cpu().numpy()
+
+# Restore the model to its old train/eval mode.
+model.train(old_train_eval_model)
+
 
--- script/my_codes/test_my_code.py
+++ script/my_codes/test_my_code.py
@@ -30,7 +30,7 @@ optimizer = optim.Adam(model.parameters(), lr=base_lr, weight_decay=weight_decay
 # Bind them together just to save some codes in the following usage.
 modules_optims = [model, optimizer]
 
-model_weight_file = '/home/niruhan/AlignedReID-Re-Production-Pytorch/model_weight.pth'
+model_weight_file = '../../model_weight.pth'
 
 map_location = (lambda storage, loc: storage)
 sd = torch.load(model_weight_file, map_location=map_location)
@@ -41,14 +41,14 @@ sys_device_ids = (0,)
 
 TVT, TMO = set_devices(sys_device_ids)
 
-name_list = ['gow.jpg', 'gunes.jpg', 'niru.jpg', 'niru.jpg']
+name_list = ['gow.jpg', 'gunes.jpg', 'niru.jpg', 'gow_query.jpg']
 
 global_features_list = []
 local_features_list = []
 
 for name in name_list:
 
-    input_image = cv2.imread(name)
+    input_image = cv2.imread('my_images/' + name)
     resized_image = cv2.resize(input_image, (128, 256))
 
     # input_image = np.asarray(Image.open('gow_query.jpg'))
@@ -93,31 +93,31 @@ global_q_g_dist = compute_dist(query_global_features_list, gallery_global_featur
 
 
 print global_q_g_dist
-
-##################
-# Local Distance #
-##################
-
-local_features_list = np.vstack((local_features_list[0], local_features_list[1], local_features_list[2], local_features_list[3]))
-normalized_local_features_list = normalize(local_features_list, axis=-1)
-
-gallery_local_features_list =normalized_local_features_list[0:3]
-query_local_features_list = np.expand_dims(normalized_local_features_list[3], axis=0)
-
-# A helper function just for avoiding code duplication.
-def low_memory_local_dist(x, y):
-    with measure_time('Computing local distance...'):
-        x_num_splits = int(len(x) / 200) + 1
-        y_num_splits = int(len(y) / 200) + 1
-        z = low_memory_matrix_op(
-            local_dist, x, y, 0, 0, x_num_splits, y_num_splits, verbose=True)
-    return z
-
-# query-gallery distance using local distance
-local_q_g_dist = low_memory_local_dist(query_local_features_list, gallery_local_features_list)
-
-print local_q_g_dist
-
-global_local_distance = global_q_g_dist + local_q_g_dist
-
-print global_local_distance
+#
+# ##################
+# # Local Distance #
+# ##################
+#
+# local_features_list = np.vstack((local_features_list[0], local_features_list[1], local_features_list[2], local_features_list[3]))
+# normalized_local_features_list = normalize(local_features_list, axis=-1)
+#
+# gallery_local_features_list =normalized_local_features_list[0:3]
+# query_local_features_list = np.expand_dims(normalized_local_features_list[3], axis=0)
+#
+# # A helper function just for avoiding code duplication.
+# def low_memory_local_dist(x, y):
+#     with measure_time('Computing local distance...'):
+#         x_num_splits = int(len(x) / 200) + 1
+#         y_num_splits = int(len(y) / 200) + 1
+#         z = low_memory_matrix_op(
+#             local_dist, x, y, 0, 0, x_num_splits, y_num_splits, verbose=True)
+#     return z
+#
+# # query-gallery distance using local distance
+# local_q_g_dist = low_memory_local_dist(query_local_features_list, gallery_local_features_list)
+#
+# print local_q_g_dist
+#
+# global_local_distance = global_q_g_dist + local_q_g_dist
+#
+# print global_local_distance
